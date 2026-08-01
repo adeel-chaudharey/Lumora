5d3e848564bcdc011e854d4984b7caf77dc3ccd6
adeel-chaudharey <adeelabbaschaudharey@gmail.com>
2026-08-01 20:59:47 +0500

    utils/supabase/middleware.ts     changed

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -16,31 +16,25 @@ export async function updateSession(request: NextRequest) {
         },
 
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
-          );
+          cookiesToSet.forEach(({ name, value }) => {
+            request.cookies.set(name, value);
+          });
 
           response = NextResponse.next({
             request,
           });
 
-          cookiesToSet.forEach(({ name, value, options }) =>
-            response.cookies.set(name, value, options)
-          );
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options);
+          });
         },
       },
     }
   );
 
-  // Refresh the auth session
+  // IMPORTANT:
+  // Refresh the auth session.
   await supabase.auth.getUser();
 
   return response;
 }
-//secured middleware
-
-
-
-
-
-
