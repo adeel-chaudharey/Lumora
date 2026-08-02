cf59720b6422bcfd243864175212cef71ba7876f
adeel-chaudharey <adeelabbaschaudharey@gmail.com>
2026-08-02 21:33:17 +0500

    check out form

---
+++ components/checkout/CheckOutForm.tsx
@@ -0,0 +1,9 @@
+export default function CheckoutForm() {
+  return (
+    <div className="rounded-3xl border border-slate-800 bg-slate-900 p-8">
+      <h2 className="text-2xl font-semibold text-white">
+       Check Out
+      </h2>
+    </div>
+  );
+}
