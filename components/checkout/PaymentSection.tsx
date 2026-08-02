a32a5450ce1a3c0a79f53eb3bddff22732eb25b8
adeel-chaudharey <adeelabbaschaudharey@gmail.com>
2026-08-02 21:35:06 +0500

    dummy payments component

---
+++ components/checkout/PaymentSection.tsx
@@ -0,0 +1,9 @@
+export default function PaymentSection() {
+  return (
+    <div className="rounded-3xl border border-slate-800 bg-slate-900 p-8">
+      <h2 className="text-2xl font-semibold text-white">
+        Payment Information
+      </h2>
+    </div>
+  );
+}
