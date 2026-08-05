7abf18fa1ea14ddd42ec7b6e43d6e1e1f1076597
adeel-chaudharey <adeelabbaschaudharey@gmail.com>
2026-08-05 12:43:26 +0500

    UTILS OF STRIPE

--- utils/stripe/server.ts
+++ utils/stripe/server.ts
@@ -7,7 +7,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY,
   {
-    apiVersion: "2025-06-30.basil",
+    apiVersion: "2026-07-29.dahlia",
     typescript: true,
   }
 );
